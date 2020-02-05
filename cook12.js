49653dcb9477b0a9cff5898823648fe36edcc1ff
Adam Moore <adamoore@gmail.com>
2020-02-04 19:17:55 -0500

    Working first draft of extension.

--- cook12.js
+++ cook12.js
@@ -1,38 +1,70 @@
-let cTotal = 0;
-let newCTotal = 0;
+/* globals Tone, chrome */
+let previousCookieCount = 0;
+let newCookieCount = 0;
+// let latestCookies;
+let streamCookies;
+
+const noiseVol = new Tone.Volume(-20);
+
+const noiseSynth = new Tone.NoiseSynth({
+  noise: { type: 'pink' },
+  envelope: { attack: 0, decay: 0.1, sustain: 1 }
+});
+
+const dripSynth = new Tone.MetalSynth({
+  frequency: 10000,
+  envelope: { attack: 0.001, decay: 1.4, release: 0.2 },
+  harmonicity: 2,
+  modulationIndex: 1,
+  resonance: 1000,
+  octaves: 1.5
+});
 
 function awaitTotalCookies() {
   return new Promise(function getCookies(resolve) {
     chrome.cookies.getAll({}, cookies => {
-      resolve(cookies.length);
+      const sustain = Math.floor(cookies.length / 10);
+      resolve(sustain);
     });
   });
 }
 
 async function cookieCall() {
-  console.log('calling');
-  const result = await awaitTotalCookies();
-  const a = result - 10;
-  console.log(`you have ${a} cookies. plus ten`);
-  return a;
+  // console.log('calling');
+  const latestCookies = await awaitTotalCookies();
+  newCookieCount = latestCookies - previousCookieCount;
+  previousCookieCount = latestCookies;
+  console.log(newCookieCount);
+  if (newCookieCount > 0) {
+    // console.log(previousCookieCount);
+    // noiseSynth.triggerAttackRelease(newCookieCount);
+    dripSynth.triggerAttackRelease(newCookieCount);
+  }
+  return newCookieCount;
 }
 
 const startBtn = document.querySelector('#start');
 const stopBtn = document.querySelector('#stop');
 
 const startStream = () => {
-  streamCookies = setInterval(cookieCall, 1000);
   console.log('start');
+  // console.log(`Playing noise for ${previousCookieCount} seconds.`);
+  streamCookies = setInterval(cookieCall, 250);
+  // noiseSynth.triggerAttackRelease(previousCookieCount);
+  // noiseSynth.chain(noiseVol, Tone.Master);
+  dripSynth.toMaster();
   startBtn.disabled = true;
+};
+
+const stopStream = () => {
+  clearInterval(streamCookies);
+  console.log('stop');
+  startBtn.disabled = false;
 };
 
 startBtn.addEventListener('click', startStream);
 
-stopBtn.addEventListener('click', () => {
-  clearInterval(streamCookies);
-  console.log('stop');
-  startBtn.disabled = false;
-});
+stopBtn.addEventListener('click', stopStream);
 
 const testFunc = function test() {
   alert('this is a test');
