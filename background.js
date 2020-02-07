0fb37d181f3d9219b4e77f3d14c2f14771a39daf
Adam Moore <adamoore@gmail.com>
2020-02-07 14:45:31 -0500

    added membrade synth following session cookies.

--- background.js
+++ background.js
@@ -2,3 +2,15 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({ url: 'index.html' });
 });
+
+let y;
+
+// function youAreHere() {
+chrome.tabs.onActivated.addListener(function activeInfo() {
+  chrome.tabs.get(activeInfo.tabId, function tab() {
+    y = tab.url;
+    console.log(`you are here: ${y}`);
+  });
+});
+// }
+// youAreHere();
--- cook12.js
+++ cook12.js
@@ -1,24 +1,47 @@
 /* globals Tone, chrome */
 let previousCookieCount = 0;
 let newCookieCount = 0;
-// let latestCookies;
+let previousSeshCount = 0;
+let newSeshCount = 0;
 let streamCookies;
-
-const noiseVol = new Tone.Volume(-20);
-
-const noiseSynth = new Tone.NoiseSynth({
-  noise: { type: 'pink' },
-  envelope: { attack: 0, decay: 0.1, sustain: 1 }
-});
+let streamSession;
 
 const dripSynth = new Tone.MetalSynth({
   frequency: 10000,
   envelope: { attack: 0.001, decay: 1.4, release: 0.2 },
   harmonicity: 2,
-  modulationIndex: 1,
-  resonance: 1000,
+  modulationIndex: 500,
+  resonance: 2000,
   octaves: 1.5
 });
+
+const flutterSynth = new Tone.MembraneSynth({
+  envelope: {
+    sustain: 0.5,
+    attackCurve: 'ripple',
+    releaseCurve: 'ripple'
+  }
+});
+
+function awaitSessionCookies() {
+  return new Promise(function getSessionCookies(resolve) {
+    chrome.cookies.getAll({ session: false }, sessionCooks => {
+      const sesh = sessionCooks.length;
+      resolve(sesh);
+    });
+  });
+}
+
+async function sessionCall() {
+  const sessionCookies = await awaitSessionCookies();
+  console.log(`there are ${sessionCookies} marked as session`);
+  newSeshCount = sessionCookies - previousSeshCount;
+  previousSeshCount = sessionCookies;
+  if (newSeshCount > 0) {
+    flutterSynth.triggerAttackRelease('C1', newCookieCount);
+  }
+  return sessionCookies;
+}
 
 function awaitTotalCookies() {
   return new Promise(function getCookies(resolve) {
@@ -32,27 +55,35 @@ function awaitTotalCookies() {
 async function cookieCall() {
   // console.log('calling');
   const latestCookies = await awaitTotalCookies();
+  // const sessionCookies = await awaitSessionCookies();
   newCookieCount = latestCookies - previousCookieCount;
   previousCookieCount = latestCookies;
   console.log(newCookieCount);
+  // console.log(`there are ${sessionCookies} marked as session.`);
+
   if (newCookieCount > 0) {
     // console.log(previousCookieCount);
     // noiseSynth.triggerAttackRelease(newCookieCount);
     dripSynth.triggerAttackRelease(newCookieCount);
   }
-  return newCookieCount;
+  // return { first: newCookieCount, second: sessionCookies };
+  return latestCookies;
 }
 
 const startBtn = document.querySelector('#start');
 const stopBtn = document.querySelector('#stop');
 
 const startStream = () => {
-  console.log('start');
-  // console.log(`Playing noise for ${previousCookieCount} seconds.`);
+  console.log('start drip');
   streamCookies = setInterval(cookieCall, 250);
-  // noiseSynth.triggerAttackRelease(previousCookieCount);
-  // noiseSynth.chain(noiseVol, Tone.Master);
   dripSynth.toMaster();
+  startBtn.disabled = true;
+};
+
+const startSession = () => {
+  console.log('start session sounds');
+  streamSession = setInterval(sessionCall, 500);
+  flutterSynth.toMaster();
   startBtn.disabled = true;
 };
 
@@ -62,9 +93,17 @@ const stopStream = () => {
   startBtn.disabled = false;
 };
 
+const stopSession = () => {
+  clearInterval(streamSession);
+  console.log('session cookie sound stopped');
+  startBtn.disabled = false;
+};
+
 startBtn.addEventListener('click', startStream);
+startBtn.addEventListener('click', startSession);
 
 stopBtn.addEventListener('click', stopStream);
+stopBtn.addEventListener('click', stopSession);
 
 const testFunc = function test() {
   alert('this is a test');
