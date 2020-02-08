57786cf12813270f734a4899c69bbcc31510b248
Adam Moore <adamoore@gmail.com>
2020-02-08 14:43:28 -0500

    added panning to secure cookies.

--- cook12.js
+++ cook12.js
@@ -1,10 +1,16 @@
 /* globals Tone, chrome */
+//Q's: How do I use asnyc results in other places i.e. envelopes?
+// counters
 let previousCookieCount = 0;
 let newCookieCount = 0;
 let previousSeshCount = 0;
 let newSeshCount = 0;
+let previousSecureCount = 0;
+let newSecureCount = 0;
+// setInterval names
 let streamCookies;
 let streamSession;
+let streamSecure;
 
 const dripSynth = new Tone.MetalSynth({
   frequency: 10000,
@@ -15,13 +21,59 @@ const dripSynth = new Tone.MetalSynth({
   octaves: 1.5
 });
 
-const flutterSynth = new Tone.MembraneSynth({
+let panner;
+
+const bassSynth = new Tone.MembraneSynth({
   envelope: {
-    sustain: 0.5,
+    sustain: 1,
+    release: 1,
     attackCurve: 'ripple',
     releaseCurve: 'ripple'
   }
 });
+
+const droneSynth = new Tone.NoiseSynth();
+
+function awaitSecureCookies() {
+  return new Promise(function getSecureCookies(resolve) {
+    chrome.cookies.getAll({ secure: true }, secureCooks => {
+      const cCure = secureCooks.length;
+      resolve(cCure);
+    });
+  });
+}
+
+function awaitNonSecureCookies() {
+  return new Promise(function getNonSecureCookies(resolve) {
+    chrome.cookies.getAll({ secure: false }, nonSecureCooks => {
+      const nCure = nonSecureCooks.length;
+      resolve(nCure);
+    });
+  });
+}
+
+async function secureCall() {
+  const secureCooks = await awaitSecureCookies();
+  const notSecureCooks = await awaitNonSecureCookies();
+  // newSecureCount = secureCooks - previousSecureCount;
+  // previousSecureCount = secureCooks;
+  console.log(
+    `there are ${secureCooks} cookies marked as secure and ${notSecureCooks} marked as not secure.`
+  );
+  if (secureCooks > notSecureCooks) {
+    panner = new Tone.Panner(-1);
+    bassSynth.connect(panner);
+    panner.toMaster();
+  } else if (notSecureCooks > secureCooks) {
+    panner = new Tone.Panner(1);
+    bassSynth.connect(panner);
+    panner.toMaster();
+  } else {
+    panner = new Tone.Panner(0);
+    bassSynth.connect(panner);
+    panner.toMaster();
+  }
+}
 
 function awaitSessionCookies() {
   return new Promise(function getSessionCookies(resolve) {
@@ -34,11 +86,12 @@ function awaitSessionCookies() {
 
 async function sessionCall() {
   const sessionCookies = await awaitSessionCookies();
-  console.log(`there are ${sessionCookies} marked as session`);
+  // console.log(`there are ${sessionCookies} marked as session`);
   newSeshCount = sessionCookies - previousSeshCount;
   previousSeshCount = sessionCookies;
+  console.log(newSeshCount);
   if (newSeshCount > 0) {
-    flutterSynth.triggerAttackRelease('C1', newCookieCount);
+    bassSynth.triggerAttackRelease('D1', newSeshCount);
   }
   return sessionCookies;
 }
@@ -83,7 +136,13 @@ const startStream = () => {
 const startSession = () => {
   console.log('start session sounds');
   streamSession = setInterval(sessionCall, 500);
-  flutterSynth.toMaster();
+  // bassSynth.toMaster();
+  startBtn.disabled = true;
+};
+
+const startSecure = () => {
+  console.log('start secure sounds');
+  streamSecure = setInterval(secureCall, 5000);
   startBtn.disabled = true;
 };
 
@@ -99,11 +158,19 @@ const stopSession = () => {
   startBtn.disabled = false;
 };
 
+const stopSecure = () => {
+  clearInterval(streamSecure);
+  console.log('secure cookie sound stopped');
+  startBtn.disabled = false;
+};
+
 startBtn.addEventListener('click', startStream);
 startBtn.addEventListener('click', startSession);
+startBtn.addEventListener('click', startSecure);
 
 stopBtn.addEventListener('click', stopStream);
 stopBtn.addEventListener('click', stopSession);
+stopBtn.addEventListener('click', stopSecure);
 
 const testFunc = function test() {
   alert('this is a test');
